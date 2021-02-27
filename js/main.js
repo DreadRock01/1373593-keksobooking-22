f6565f1f230ddf4019a39a2404800b3951ae0eca
DreadRock01 <dreadrock01@users.noreply.github.com>
2021-02-27 16:54:23 +0500

    Вынос генерации объяв

--- js/main.js
+++ js/main.js
@@ -1,2 +1,16 @@
-import './data.js';
-import './card.js';
+import {
+  CARDS_COUNT,
+  createSimilarAds
+} from './data.js';
+
+import {
+  createCard,
+  cardsOnCanvas
+} from './card.js';
+
+import './form.js';
+
+let similarAds = createSimilarAds(CARDS_COUNT);
+let similarCards = createCard(similarAds[0]);
+
+cardsOnCanvas.appendChild(similarCards);
