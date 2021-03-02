f9092b31972827bb81a2657f6ae6346f6ee304ee
DreadRock01 <dreadrock01@users.noreply.github.com>
2021-03-02 22:40:15 +0500

    Реализация активного и неактивного состояния страницы

--- js/form.js
+++ js/form.js
@@ -5,16 +5,47 @@ const minPriceOfRent = {
   palace: 10000,
 }
 
-// const mapFilter = document.querySelectorAll('.map__filter');
-// const mapFeatures = document.querySelector('.map__features');
+const adForm = document.querySelector('.ad-form');
+const adFormFieldsets = document.querySelectorAll('fieldset');
+const mapFilters = document.querySelector('.map__filters');
 const housingType = document.querySelector('#type');
 const pricePerNight = document.querySelector('#price');
 const timeIn = document.querySelector('#timein');
 const timeOut = document.querySelector('#timeout');
 
+
+/**
+ * Функция блокировки взаимодействия с интерактивными элементами
+ */
+const setFormDisabled = () => {
+  adForm.classList.add('ad-form--disabled');
+  adFormFieldsets.forEach((elem) => {
+    elem.setAttribute('disabled','');
+  });
+  mapFilters.classList.add('map__filters--disabled');
+  [...mapFilters.elements].forEach((elem) => {
+    elem.setAttribute('disabled','');
+  });
+}
+setFormDisabled();
+
+
+/**
+ * Функция активации взаимодействия с интерактивными элементами
+ */
+const setFormEnabled = () => {
+  adForm.classList.remove('ad-form--disabled');
+  adFormFieldsets.forEach((elem) => {
+    elem.removeAttribute('disabled','');
+  });
+  mapFilters.classList.remove('map__filters--disabled');
+  [...mapFilters.elements].forEach((elem) => {
+    elem.removeAttribute('disabled','');
+  });
+}
+
 housingType.addEventListener('change', (evt) => {
   pricePerNight.min = pricePerNight.placeholder = minPriceOfRent[evt.target.value];
-  // alert(`evt.type ${'на'} evt.currentTarget`);
 })
 
 timeIn.addEventListener('change', (evt) => {
@@ -24,3 +55,7 @@ timeIn.addEventListener('change', (evt) => {
 timeOut.addEventListener('change', (evt) => {
   timeIn.value = evt.target.value;
 })
+
+export {
+  setFormEnabled
+};
