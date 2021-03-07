7c140f2c0f947b15715298ccea481349c7a31806
DreadRock01 <dreadrock01@users.noreply.github.com>
2021-03-07 21:52:25 +0500

    Упрощение функции. Перенос части кода в блок валидации

--- js/form.js
+++ js/form.js
@@ -1,61 +1,34 @@
-const minPriceOfRent = {
-  bungalow: 0,
-  flat: 1000,
-  house: 5000,
-  palace: 10000,
-}
-
 const adForm = document.querySelector('.ad-form');
 const adFormFieldsets = document.querySelectorAll('fieldset');
 const mapFilters = document.querySelector('.map__filters');
-const housingType = document.querySelector('#type');
-const pricePerNight = document.querySelector('#price');
-const timeIn = document.querySelector('#timein');
-const timeOut = document.querySelector('#timeout');
-
 
 /**
- * Функция блокировки взаимодействия с интерактивными элементами
+ * Функция переключения возможности взаимодействия с интерактивными элементами
+ * @param {string} isEnabled — состояние формы
  */
-const setFormDisabled = () => {
-  adForm.classList.add('ad-form--disabled');
-  adFormFieldsets.forEach((elem) => {
-    elem.setAttribute('disabled','');
-  });
-  mapFilters.classList.add('map__filters--disabled');
-  [...mapFilters.elements].forEach((elem) => {
-    elem.setAttribute('disabled','');
-  });
+const setFormState = (isEnabled) => {
+  if (!isEnabled) {
+    adForm.classList.add('ad-form--disabled');
+    adFormFieldsets.forEach((elem) => {
+      elem.setAttribute('disabled', '');
+    });
+    mapFilters.classList.add('map__filters--disabled');
+    [...mapFilters.elements].forEach((elem) => {  // попробовал без деструктуризации - возвращает forEach is not a function
+      elem.setAttribute('disabled', '');
+    });
+  } else {
+    adForm.classList.remove('ad-form--disabled');
+    adFormFieldsets.forEach((elem) => {
+      elem.removeAttribute('disabled', '');
+    });
+    mapFilters.classList.remove('map__filters--disabled');
+    [...mapFilters.elements].forEach((elem) => {
+      elem.removeAttribute('disabled', '');
+    });
+  }
 }
-setFormDisabled();
-
-
-/**
- * Функция активации взаимодействия с интерактивными элементами
- */
-const setFormEnabled = () => {
-  adForm.classList.remove('ad-form--disabled');
-  adFormFieldsets.forEach((elem) => {
-    elem.removeAttribute('disabled','');
-  });
-  mapFilters.classList.remove('map__filters--disabled');
-  [...mapFilters.elements].forEach((elem) => {
-    elem.removeAttribute('disabled','');
-  });
-}
-
-housingType.addEventListener('change', (evt) => {
-  pricePerNight.min = pricePerNight.placeholder = minPriceOfRent[evt.target.value];
-})
-
-timeIn.addEventListener('change', (evt) => {
-  timeOut.value = evt.target.value;
-})
-
-timeOut.addEventListener('change', (evt) => {
-  timeIn.value = evt.target.value;
-})
+setFormState(false);
 
 export {
-  setFormEnabled
+  setFormState
 };
