9a0e90885e6823286a64e9cbe7f5b340111e1a25
DreadRock01 <dreadrock01@users.noreply.github.com>
2021-03-18 12:06:05 +0500

    Новый модуль под модал

---
+++ js/modal.js
@@ -0,0 +1,58 @@
+import {
+  isEscEvent,
+  isEnterEvent
+} from './util.js';
+
+const successMessageTemplate = document.querySelector('#success').content.querySelector('.success');
+const errorMessageTemplate = document.querySelector('#error').content.querySelector('.error');
+const main = document.querySelector('main');
+
+/**
+ * Функция скрытия модала после отправки формы
+ */
+const modalHide = () => {
+  const modal = main.querySelector('.success') || main.querySelector('.error');
+  modal.remove();
+  document.removeEventListener('keydown', onModalKeyPress);
+};
+
+/**
+ * Клавиши
+ * @param {object} evt объект события
+ */
+const onModalKeyPress = (evt) => {
+  isEscEvent(evt, modalHide);
+  isEnterEvent(evt, modalHide);
+};
+
+/**
+ * Функция показа сообщения об успешной отправке формы
+ */
+const createSuccessMessage = () => {
+  const successMessage = successMessageTemplate.cloneNode(true);
+  successMessage.style.zIndex = 1000;
+  main.append(successMessage);
+  document.addEventListener('keydown', onModalKeyPress);
+  successMessage.addEventListener('click', modalHide);
+}
+
+/**
+ * Функция показа сообщения об ошибке
+ */
+const createErrorMessage = () => {
+  const errorMessage = errorMessageTemplate.cloneNode(true);
+  const errorBtn = errorMessage.querySelector('.error__button');
+  errorMessage.style.zIndex = 1000;
+  main.append(errorMessage);
+  document.addEventListener('keydown', onModalKeyPress);
+  errorBtn.addEventListener('click', modalHide);
+  errorMessage.addEventListener('click', modalHide);
+}
+
+export {
+  onModalKeyPress,
+  modalHide,
+  createSuccessMessage,
+  createErrorMessage
+}
+
