232b7816a46f31b16f9b0d9cd871a373ce833167
DreadRock01 <dreadrock01@users.noreply.github.com>
2021-03-26 19:54:53 +0500

    Сборка. Remastered

--- source/js/util.js
+++ source/js/util.js
@@ -6,12 +6,18 @@ const ERR_SHOW_TIME = 5000;
  * @param {array} words массив с вариантами слов
  * @returns {string} полученное слово в зависимости от числа
  */
-const changeWordsFormat = (n, words) => {
-  n = Math.abs(n) % 100;
-  const n1 = n % 10;
-  if (n > 10 && n < 20) return words[2];
-  if (n1 > 1 && n1 < 5) return words[1];
-  if (n1 === 1) return words[0];
+const changeWordsFormat = (number, words) => {
+  number = Math.abs(number) % 100;
+  const number1 = number % 10;
+  if (number > 10 && number < 20) {
+    return words[2];
+  }
+  if (number1 > 1 && number1 < 5) {
+    return words[1];
+  }
+  if (number1 === 1) {
+    return words[0];
+  }
   return words[2];
 }
 
